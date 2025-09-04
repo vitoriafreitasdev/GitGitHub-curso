73e389f20b25485d52b113d81d7f97bad24fb4b4
Vitória Freitas <vitoriafreitas110903@gmail.com>
2025-09-04 15:03:17 -0300

    ok

--- exercicios/teste.js
+++ exercicios/teste.js
@@ -1,14 +1,20 @@
 
-async function postarDados(url, product){
-    const res = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            body: JSON.stringify(product)
-        }
-    })
-}
-let setproducts
+// async function postarDados(url, product){
+//     const res = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             body: JSON.stringify(product)
+//         }
+//     })
+// }
+// let setproducts
 
-setproducts = ((prev) => [...prev, product])
+// setproducts = ((prev) => [...prev, product])
+
+let num = 8
+
+let res = num **= 2 
+
+console.log(res)
 
